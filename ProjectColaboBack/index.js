dd9a84a96081d2e387d13d545cd213af46493632
Raul <rulanxa@gmail.com>
2024-06-12 13:10:57 +0200

    fix: fixed module requirement

--- ProjectColaboBack/index.js
+++ ProjectColaboBack/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const users = require('./users');
+const users = require('./src/users');
 
 const app = express();
 const PORT = 5000;
