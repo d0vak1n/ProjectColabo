b8494e59d51b327c8bd04fa388a193ea2f05fb06
Raul <rulanxa@gmail.com>
2024-06-17 17:12:57 +0200

    feat: login and register connected with db

--- ProjectColaboBack/src/users.js
+++ ProjectColaboBack/src/users.js
@@ -2,26 +2,54 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 var db = require('./dbconnection');
 
-const users = []; // This should be replaced with a real database in production
-
 const register = async (req, res) => {
   const { email, password, firstName, lastName, linkedin, github  } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  users.push({ email, password: hashedPassword });
-  console.log(users);
-  res.status(201).send('User registered');
+
+  // Comprovamos si el email ya ha sido registrado
+  db.query('SELECT * FROM estudiantes WHERE email = ?', [email], async (error, results) => {
+    if (error) {
+      console.log(error);
+    }
+
+    if (results.length > 0) {
+      return res.status(409).send('Este email ya ha sido registrado');
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      db.query('INSERT INTO estudiantes SET ?', {email: email, password: hashedPassword, nombre: firstName, apellido: lastName, github: github, linkdein: linkedin}, (error, results) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(results);
+          res.status(201).send('Usuario registrado correctamente');
+        }
+      });
+    }
+  });
 };
 
 const login = async (req, res) => {
   const { email, password } = req.body;
-  const user = users.find(u => u.email === email);
 
-  if (!user || !(await bcrypt.compare(password, user.password))) {
-    return res.status(401).send('Invalid credentials');
-  }
+  db.query('SELECT * FROM estudiantes WHERE email = ?', [email], async (error, results) => {
+    if (error) {
+      console.log(error);
+      return res.status(500).send('Error al consultar la base de datos');
+    }
 
-  const token = jwt.sign({ email: user.email }, 'your_jwt_secret');
-  res.json({ token });
+    if (results.length === 0) {
+      return res.status(401).send('Correo y/o contraseña incorrectos');
+    }
+
+    const user = results[0];
+
+    if (!(await bcrypt.compare(password, user.password))) {
+      return res.status(401).send('Correo y/o contraseña incorrectos');
+    }
+
+    const token = jwt.sign({ email: user.email }, 'your_jwt_secret');
+    res.json({ token });
+  });
 };
 
 module.exports = { register, login };
