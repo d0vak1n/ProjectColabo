d5cff666f094bb219c24a9730a1e3cd2763d2115
Raul <rmoreno@ctisoluciones.com>
2024-10-22 13:03:42 +0200

    feat: tipos añadidos

--- ProjectColaboBack/src/testdb.ts
+++ ProjectColaboBack/src/testdb.ts
@@ -1,6 +1,7 @@
+import { MysqlError, FieldInfo } from 'mysql';
 import db from './functions/dbconnection';
 
-db.query('SELECT 1', (error, results, fields) => {
+db.query('SELECT 1', (error: MysqlError | null, results: any, fields: FieldInfo[] | undefined) => {
   if (error) throw error;
   console.log('La conexión a la base de datos es exitosa!');
 });
