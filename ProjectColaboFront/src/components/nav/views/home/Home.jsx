c2958b061d523aa20c20fc8537b41f763cf4b436
Raul <rulanxa@gmail.com>
2024-12-07 01:43:01 +0100

    feat: incluye refresco al crear nuevo proyecto

--- ProjectColaboFront/src/components/nav/views/home/Home.jsx
+++ ProjectColaboFront/src/components/nav/views/home/Home.jsx
@@ -22,7 +22,7 @@ export default function Home() {
             .catch(error => {
                 console.error('Error:', error);
             });
-    }, []);
+    }, [openModalNuevoProyecto]);
 
     const abrirModalNuevoProyecto = () => {
         handleOpen();
